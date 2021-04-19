d74704cac54bb1b08e09cf52d548eb6a788145bc
dix12uodkean <guaihangxietui@126.com>
2021-04-19 14:31:13 +0800

    添加登陆功能

---
+++ Git/Git/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Git
+//
+//  Created by lanou on 16/1/8.
+//  Copyright © 2016年 CJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Git/Git/LoginViewController.m
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.m
+//  Git
+//
+//  Created by lanou on 16/1/8.
+//  Copyright © 2016年 CJ. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+#pragma mark 完善登陆功能
+}
+
+@end
